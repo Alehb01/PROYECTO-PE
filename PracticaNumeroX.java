93ba509acf5f38826f0d1ac9d3af08a1cdf10090
ZusetteV17 <Melissarobles2004@hotmail.com>
2025-01-27 08:13:55 -0600

    Se realizo un cambio en el archivo

--- PracticaNumeroX.java
+++ PracticaNumeroX.java
@@ -4,7 +4,7 @@ public class PracticaNumeroX {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese un numero");
+        System.out.println("Ingrese un numero"); //pedirle al usuario un número
         int numero = scanner.nextInt();
 
         System.out.println("El numero: " + numero + " tiene " + Integer.toString(numero).length() + " cifras");
