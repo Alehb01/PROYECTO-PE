b40c334b3b1202b36244659fe89b5b6fb8a82946
Luis-Wicho <sanchezlopezluisangel07@gmail.com>
2025-02-09 20:56:18 -0600

    Se agrego cajas de texto y botones al proyecto

--- ProyectoZAL.java
+++ ProyectoZAL.java
@@ -17,16 +17,22 @@ public class ProyectoZAL {
         JLabel etiqueta_3 = new JLabel("Teléfono: ");
         JTextField texto_1 = new JTextField();
         JTextField texto_2 = new JTextField();
+        JButton boton1 = new JButton("Agregar contacto");
+        JButton boton2 = new JButton("Editar contacto");
       
         etiqueta_2.setBounds(0, 10, 200,30);
         etiqueta_3.setBounds(0, 50, 200,30);
         texto_1.setBounds(220, 10, 200, 30);
         texto_2.setBounds(220, 50, 200, 30);
+        boton1.setBounds(150, 85, 200, 30);
+        boton2.setBounds(150, 115, 200, 30);
        
         frame.add(etiqueta_2);
         frame.add(etiqueta_3);
         frame.add(texto_1);
         frame.add(texto_2);
+        frame.add(boton1);
+        frame.add(boton2);
  
 
         //hacer la ventana visible
